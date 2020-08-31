1e50caf220ad029f9cbd1cada17350c3b7d74451
realheim48 <realheim.48@gmail.com>
2020-09-01 04:10:15 +0900

    Player movement implemented
    Have to fix ridigbody problem - collider dug into another collider.

---
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rigidbody;
+
+    private IMoveInput moveInput;
+
+    [SerializeField] private float moveSpeed;
+
+    private void Awake()
+    {
+        moveInput = FindObjectOfType<PlayerInput>();
+    }
+
+    private void Update()
+    {
+        Move(rigidbody, moveInput.MoveVector, moveSpeed * Time.deltaTime);
+    }
+
+    private void Move(Rigidbody2D rigidbody, Vector2 vec, float speed)
+    {
+        // TODO: Player collider dug in to another collider
+        rigidbody.position += vec * speed;
+    }
+    private void Move(Transform transform, Vector3 vec, float speed)
+    {
+        transform.position += vec * speed;
+    }
+}
