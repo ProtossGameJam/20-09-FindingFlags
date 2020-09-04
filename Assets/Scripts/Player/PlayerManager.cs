f3a00f648ea5f16e4af3c9e635367b9b8b8e7fba
realheim48 <realheim.48@gmail.com>
2020-09-04 21:13:58 +0900

    Player movement script test

---
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    [SerializeField] private SpriteSortingOrder orderFixer;
+    
+    private void Update()
+    {
+        orderFixer.SortingOrderFix();
+    }
+}
