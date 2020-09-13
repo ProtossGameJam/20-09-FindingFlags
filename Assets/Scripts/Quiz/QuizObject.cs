492809611aa1f21b9ba6af1d1b91a82f0e458bf9
SmokyOnion <tomekichi1551@gmail.com>
2020-09-13 17:30:27 +0900

    Created Quiz Obejct

---
+++ Assets/Scripts/Quiz/QuizObject.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Quiz", menuName = "Quiz/Quiz Element Create", order = 0)]
+public class QuizObject : ScriptableObject
+{
+    [System.Serializable]
+    public class QuizInfo
+    {
+        public int itemID;
+        [Space(10), Multiline]
+        public string question;
+        [Space(5)]
+        public string choice1;
+        public string choice2;
+        public string choice3;
+        [Space(5)]
+        public int correct;
+    }
+
+    [SerializeField]
+    public List<QuizInfo> QuizList;
+    public QuizInfo GetQuizInfo(int id)
+    {
+        return QuizList.Find(info => info.itemID == id);
+    }
+}
