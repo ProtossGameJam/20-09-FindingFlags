5be2e2e0112f27d26f211b275dfd837c077fd2e6
SmokyOnion <tomekichi1551@gmail.com>
2020-09-13 22:18:31 +0900

    Develop Quiz Logic 50%
    - 퀴즈 코드 적용
    - 퀴즈 UI 생성
    - 퀴즈용 스크립터블 오브젝트 생성

--- Assets/Scripts/Dialogue/DialogueManager.cs
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -25,6 +25,10 @@ public class DialogueManager : MonoBehaviour, IInteractable
             if (currentIndex < element.sentence.Count) {
                 textBubble.Write(element.sentence[currentIndex++].content, typeSpeed);
             }
+            else if(element.sentence[currentIndex - 1].eventCode != "")
+            {
+                DoEventCode(element.sentence[currentIndex - 1].eventCode);
+            }
             else {
                 print("End Dialogue");
                 // End group dialogue
@@ -32,6 +36,24 @@ public class DialogueManager : MonoBehaviour, IInteractable
         }
         else {
             textBubble.writeElement.WriteAllAndDestroy();
+        }
+    }
+
+
+    /* Purpose: 이벤트 코드를 판독하고 실행하는 함수.
+     * Variable: 이벤트 코드
+     * Notice: PrintDialogue() 에서 호출될 것임.
+     */
+    private void DoEventCode(string eventCode)
+    {
+        switch(eventCode) 
+        {
+            case "#q":
+                textBubble.gameObject.SetActive(false); //SmokyOnion : TextBubble 에 대한 이해도가 부족하여 디버그용으로 넣은 코드.
+                FindObjectOfType<QuizManager>().Quiz_StartQuiz(this, transform.GetChild(0).position, 0); //Smokyonion : 위치값을 수작업으로 찾고 있음. 변수로 등록할 여부도 고려..
+                break;
+
+            default: print("Error : 이벤트 코드가 이상합니다. (~DoEventCode())"); return;
         }
     }
 }
---
+++ Assets/Scripts/Quiz/QuizManager.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+/* Purpose: 퀴즈와 관련된 전반적인 것들을 관리해주는 클래스
+ * Notice: 
+ */
+public class QuizManager : MonoBehaviour
+{
+    [Header("Object")]
+    [SerializeField] private QuizObject obj_quizObj;    //퀴즈 스크립터블 오브젝트
+    [SerializeField] private GameObject obj_pivot;      //퀴즈 오브젝트 컨테이너 
+
+    [Header("Variable")]
+    [ReadOnly] [SerializeField] private int var_correctNumber;     //퀴즈의 정답 번호
+
+
+    [Header("UI")]
+    [SerializeField] private TMP_Text ui_description;    //퀴즈 내용 
+    [SerializeField] private TMP_Text ui_answerText1;    //버튼 1번
+    [SerializeField] private TMP_Text ui_answerText2;    //버튼 2번
+    [SerializeField] private TMP_Text ui_answerText3;    //버튼 3번
+
+    private DialogueManager obj_currentDm;               //최근에 호출받은 DialogueManager
+
+
+
+    /* Purpose: 새로운 퀴즈를 준비하고 시작하는 함수
+     * Variable: 호출한 오브젝트, 퀴즈 UI 위치값, 퀴즈 ID값.
+     * Notice: DialogueManager 에서 Code를 감지하고 실행하게 됨.
+     */
+    public void Quiz_StartQuiz(DialogueManager from, Vector3 position, int id)
+    {
+        QuizObject.QuizInfo newQuiz = obj_quizObj.GetQuizInfo(id);
+
+        ui_description.text = newQuiz.question;
+        ui_answerText1.text = newQuiz.choice1;
+        ui_answerText2.text = newQuiz.choice2;
+        ui_answerText3.text = newQuiz.choice3;
+        var_correctNumber = newQuiz.correct;
+
+        obj_pivot.SetActive(true);
+        transform.position = position;
+        obj_currentDm = from;
+    }
+
+    /* Purpose: 퀴즈의 정답을 맞췄을때 호출하는 함수.
+     * Variable: 
+     * Notice: 
+     */
+    private void Quiz_Correct()
+    {
+        Debug.Log("정답!");
+    }
+
+    /* Purpose: 퀴즈의 오답을 선택했을때 호출하는 함수.
+     * Variable: 
+     * Notice: 
+     */
+    private void Quiz_Incorrect()
+    {
+        Debug.Log("오답!");
+    }
+
+
+    /* Purpose: 퀴즈를 풀지않고 X를 눌렀을 때 호출하는 함수.
+     * Variable: 
+     * Notice: 
+     */
+    private void Quiz_Quit()
+    {
+        obj_pivot.SetActive(false);
+    }
+
+
+    #region UI
+
+    /* Purpose: 버튼 클릭 이벤트용 함수
+     * Variable: 클릭된 버튼의 아이디 값 (1~4)
+     * Notice: 버튼 UI가 사용하게 될 것임.
+     */
+    public void UI_ButtonAnswer(int buttonId)
+    {
+        switch(buttonId) 
+        {
+            case 1: case 2: case 3: 
+                if(buttonId == var_correctNumber)
+                {
+                    Quiz_Correct();
+                }
+                else
+                {
+                    Quiz_Incorrect();
+                }
+                break;
+
+            case 4: 
+                Quiz_Quit(); break;
+        }
+    }
+
+    #endregion
+}
