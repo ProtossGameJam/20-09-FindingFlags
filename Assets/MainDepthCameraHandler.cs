9f1b098f84bee1b91fdbb92e8ea019badc96b6c6
gimganghwi <gimganghwi@gmail.com>
2020-09-17 11:34:17 +0900

    메인 UI 그래픽 및 에니메이션 작업

---
+++ Assets/MainDepthCameraHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainDepthCameraHandler : MonoBehaviour
+{
+    Vector3 originalPoisition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalPoisition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = originalPoisition + (Input.mousePosition - new Vector3(Screen.width/2, Screen.height/2, 0))/1200;
+    }
+}
